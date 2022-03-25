b40c42ad5943fd0b8846f2fca527552d20b41e55
Josh <joshualawson8@gmail.com>
2022-03-24 18:45:57 -0700

    AppFactory to Scala

--- src/main/scala/mvc/AppFactory.scala
+++ src/main/scala/mvc/AppFactory.scala
@@ -1,11 +1,10 @@
-//package mvc
-//
-//trait AppFactory:
-//    def makeModel(): Model
-//    //    public Model makeModel()
-////    public View makeView(Model m)
-////    public String getTitle()
-////    public String[] getHelp()
-////    public String about()
-////    public String[] getEditCommands()
-////    public Command makeEditCommand(Model m, String name, Object source)
+package mvc
+
+trait AppFactory:
+    def makeModel(): Model
+    def makeView(): View
+    def getTitle: String
+    def getHelp: List[String]
+    def about(): String
+    def getEditCommands: List[String]
+    def makeEditCommand(m: Model, name: String, source: Any): Unit
