e0f94cee44e0bb852907c0cb86f4ce691149c644
Josh <joshualawson8@gmail.com>
2022-03-24 18:46:21 -0700

    Command to Scala

--- src/main/scala/mvc/Command.scala
+++ src/main/scala/mvc/Command.scala
@@ -1,11 +1,4 @@
-//package mvc;
-//
-//public abstract class Command {
-//
-//    protected Model model;
-//
-//    public Command(Model model){
-//        this.model =  model;
-//    }
-//    public abstract void execute() throws Exception;
-//}
+package mvc
+
+trait Command(model: Model):
+  def execute(): Unit
