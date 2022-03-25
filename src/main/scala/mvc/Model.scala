1f2f12527fddc1e801851a4fc019411fad5824aa
Josh <joshualawson8@gmail.com>
2022-03-24 18:47:32 -0700

    convert Model to Scala

--- src/main/scala/mvc/Model.scala
+++ src/main/scala/mvc/Model.scala
@@ -1,10 +1,14 @@
 package mvc
 
-trait Model(var unsavedChanges: Boolean = false, var filename: String = ""): //extends Bean
-  def setChanges(b: Boolean) =
+import tools._
+
+trait Model(var unsavedChanges: Boolean = false, var filename: String = "") extends Bean:
+  def setChanges(b: Boolean): Unit =
     unsavedChanges = b
-  def setFileName(s: String) =
+
+  def setFileName(s: String): Unit =
     filename = s
-  def changed() =
+
+  def changed(): Unit =
     unsavedChanges = true
     println("changed")
